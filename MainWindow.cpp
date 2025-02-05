0834ca0686457f8625f8041af3a7cb0f1a7dc715
Kyle Romero <kgromero@gmail.com>
2025-02-05 16:25:31 -0500

    Update debug output

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -141,12 +141,9 @@ void MainWindow::selectFolder()
                 QString fullPath = dir + QDir::separator() + fileName;
                 if (fileModel->shouldIncludeFile(fullPath)) {
                     selectedFiles.insert(fullPath);
-                    qDebug() << "Auto adding:" << fullPath;
                 }
             }
             
-            qDebug() << "Auto selected files:" << selectedFiles.size();
-
             // Programmatically select these files in the tree
             fileTreeView->selectionModel()->clearSelection();
             for (const QString &filePath : selectedFiles) {
